6fd3a458ab07afd22bd613a41a314c552aad4bcf
jaeo-jeong <asdfgh8579@gmail.com>
2025-12-25 00:25:13 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase 설정
+// 이 파일은 Firebase 콘솔에서 프로젝트 설정 > 일반 > 앱 추가에서 얻은 설정값으로 교체해야 합니다
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_oHHQSw8GXrDUeYgn7u4m3L5ml2OArXk",
+    authDomain: "jeotja-fc.firebaseapp.com",
+    projectId: "jeotja-fc",
+    storageBucket: "jeotja-fc.firebasestorage.app",
+    messagingSenderId: "974948021620",
+    appId: "1:974948021620:web:daa7a24bc63d1d79d1a821"
+  };
+
+// Firebase 초기화는 index.html에서 수행됩니다
+
